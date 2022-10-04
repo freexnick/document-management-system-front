ca71758afaa155c561edd46525f15d8beb17bbd0
Nika <freexnick@yahoo.com>
2022-10-04 22:51:51 +0400

    refactor:App_routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import { UserList } from "./User/UserList";
-import { Files } from "./Files";
 import { Nav } from "./Nav";
 import { Auth } from "./Auth/Auth";
 import { HandleUser } from "./User/HandleUser";
 import { useAuthContext } from "./Auth/AuthContext";
 import { useEffect } from "react";
+import { Upload } from "./documents/Upload";
 
 export const App = () => {
-  const { isLogged } = useAuthContext();
+  const { status } = useAuthContext();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isLogged?.status) navigate("/login");
-  }, [isLogged?.status]);
+    if (!status?.isLogged) navigate("/login");
+  }, [status?.isLogged]);
 
   return (
     <>
-      {isLogged?.status && <Nav />}
       <Routes>
-        <>
-          <Route path="/" element={<Nav />} />
-          <Route path="/login" element={<Auth />} />
-          <Route path="/user" element={<UserList />} />
-          <Route path="/files" element={<Files />} />
-          <Route path="/add" element={<HandleUser />} />
-          <Route path="/update/:email" element={<HandleUser />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </>
+        {status?.isLogged ? (
+          <>
+            <Route path="/" element={<Nav status={status} />} />
+            <Route path="/user" element={<UserList />} />
+            <Route path="/add" element={<HandleUser />} />
+            <Route
+              path="/update/:email"
+              element={<HandleUser status={status} />}
+            />
+            <Route
+              path="/documents/:email"
+              element={<Upload status={status} />}
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Auth />} />
+            <Route path="/login" element={<Auth />} />
+          </>
+        )}
         )
       </Routes>
     </>
