241062d3071dcc072ba00a62f40e7e9b94dfb9f2
Nika <freexnick@yahoo.com>
2022-10-14 19:27:32 +0400

    refactor:auth

--- src/Auth/AuthContext.jsx
+++ src/Auth/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login, logout } from "../api/login";
 
